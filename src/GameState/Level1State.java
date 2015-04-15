21f54a4b25d36dfa6ebf7703ddbf300916d1f96e
Nissinen <frazento@hotmail.com>
2015-04-15 16:18:03 +0300

    no nyt on kaikki palat paikallaan
    dodih

--- src/GameState/Level1State.java
+++ src/GameState/Level1State.java
@@ -88,274 +88,274 @@ public class Level1State extends GameState {
         s[4] = new Saw (1070, 110);
         s[5] = new Saw (1000, 110);
         // Alaosasto vasemmalta oikealle
-        b[0] = new Block (-10, 560, 1);
+        b[0] = new Block (-10, 560, 0);
         b[1] = new Block (-10, 590, 1);
-        b[2] = new Block (-10, 620, 0);
-        b[3] = new Block (-10, 650, 0);
-        b[4] = new Block (-10, 680, 0);
+        b[2] = new Block (-10, 620, 1);
+        b[3] = new Block (-10, 650, 1);
+        b[4] = new Block (-10, 680, 1);
         b[5] = new Block (-10, 710, 1);
         
-        b[6] = new Block (20, 560, 1);
+        b[6] = new Block (20, 560, 0);
         b[7] = new Block (20, 590, 1);
         b[8] = new Block (20, 620, 1);
         b[9] = new Block (20, 650, 1);
         b[10] = new Block (20, 680, 1);
         b[11] = new Block (20, 710, 1);
         
-        b[12] = new Block (50, 560, 1);
-        b[13] = new Block (50, 590, 1);
-        b[14] = new Block (50, 620, 1);
-        b[15] = new Block (50, 650, 1);
+        b[12] = new Block (50, 560, 7);
+        b[13] = new Block (50, 590, 14);
+        b[14] = new Block (50, 620, 14);
+        b[15] = new Block (50, 650, 15);
         b[16] = new Block (50, 680, 1);
         b[17] = new Block (50, 710, 1);
         
-        b[18] = new Block (80, 650, 1);
+        b[18] = new Block (80, 650, 0);
         b[19] = new Block (80, 680, 1);
         b[20] = new Block (80, 710, 1);
         
-        b[21] = new Block (110, 650, 1);
+        b[21] = new Block (110, 650, 0);
         b[22] = new Block (110, 680, 1);
         b[23] = new Block (110, 710, 1);
         
-        b[24] = new Block (140, 650, 1);
+        b[24] = new Block (140, 650, 0);
         b[25] = new Block (140, 680, 1);
         b[26] = new Block (140, 710, 1);
         
-        b[27] = new Block (170, 650, 1);
+        b[27] = new Block (170, 650, 0);
         b[28] = new Block (170, 680, 1);
         b[29] = new Block (170, 710, 1);
         
-        b[30] = new Block (200, 650, 1);
+        b[30] = new Block (200, 650, 0);
         b[31] = new Block (200, 680, 1);
         b[32] = new Block (200, 710, 1);
         
-        b[33] = new Block (230, 620, 1);
-        b[34] = new Block (230, 650, 1);
-        b[35] = new Block (230, 680, 1);
-        b[36] = new Block (230, 710, 1);
+        b[33] = new Block (230, 620, 5);
+        b[34] = new Block (230, 650, 2);
+        b[35] = new Block (230, 680, 12);
+        b[36] = new Block (230, 710, 12);
         
         //piikki kuilu
         b[37] = new Block (265, 720, 1); // ulkona näytöltä
         
-        b[38] = new Block (300, 620, 1);
-        b[39] = new Block (300, 650, 1);
-        b[40] = new Block (300, 680, 1);
-        b[41] = new Block (300, 710, 1);
+        b[38] = new Block (300, 620, 5);
+        b[39] = new Block (300, 650, 3);
+        b[40] = new Block (300, 680, 16);
+        b[41] = new Block (300, 710, 16);
         
-        b[42] = new Block (330, 650, 1);
+        b[42] = new Block (330, 650, 0);
         b[43] = new Block (330, 680, 1);
         b[44] = new Block (330, 710, 1);
         
-        b[45] = new Block (360, 650, 1);
-        b[46] = new Block (360, 680, 1);
-        b[47] = new Block (360, 710, 1);
+        b[45] = new Block (360, 650, 2);
+        b[46] = new Block (360, 680, 12);
+        b[47] = new Block (360, 710, 12);
         
         //ensimmoisen rotkon kohta ( sirkkeli päällä) mallissa
         
-        b[48] = new Block (500, 590, 1);
-        b[49] = new Block (500, 620, 1);
-        b[50] = new Block (500, 650, 1);
-        b[51] = new Block (500, 680, 1);
-        b[52] = new Block (500, 710, 1);
+        b[48] = new Block (500, 590, 3);
+        b[49] = new Block (500, 620, 16);
+        b[50] = new Block (500, 650, 16);
+        b[51] = new Block (500, 680, 16);
+        b[52] = new Block (500, 710, 16);
         
-        b[53] = new Block (530, 590, 1);
+        b[53] = new Block (530, 590, 0);
         b[54] = new Block (530, 620, 1);
         b[55] = new Block (530, 650, 1);
         b[56] = new Block (530, 680, 1);
         b[57] = new Block (530, 710, 1);
         
-        b[58] = new Block (560, 590, 1);
-        b[59] = new Block (560, 620, 1);
-        b[60] = new Block (560, 650, 1);
-        b[61] = new Block (560, 680, 1);
-        b[62] = new Block (560, 710, 1);
+        b[58] = new Block (560, 590, 2);
+        b[59] = new Block (560, 620, 12);
+        b[60] = new Block (560, 650, 12);
+        b[61] = new Block (560, 680, 12);
+        b[62] = new Block (560, 710, 12);
         
         // rappusten alotuis alusta
         
-        b[63] = new Block (660, 650, 1);
-        b[64] = new Block (660, 680, 1);       
-        b[65] = new Block (660, 710, 1);
+        b[63] = new Block (660, 650, 3);
+        b[64] = new Block (660, 680, 16);       
+        b[65] = new Block (660, 710, 16);
         
-        b[66] = new Block (690, 650, 1);
-        b[67] = new Block (690, 680, 1);
-        b[68] = new Block (690, 710, 1);
+        b[66] = new Block (690, 650, 2);
+        b[67] = new Block (690, 680, 12);
+        b[68] = new Block (690, 710, 12);
         
         // rappuset
         
-        b[69] = new Block (820, 650, 1);
-        b[70] = new Block (820, 680, 1);
-        b[71] = new Block (820, 710, 1);
+        b[69] = new Block (820, 650, 5);
+        b[70] = new Block (820, 680, 6);
+        b[71] = new Block (820, 710, 6);
         
-        b[72] = new Block (920, 590, 1);
-        b[73] = new Block (920, 620, 1);    
-        b[74] = new Block (920, 650, 1);
-        b[75] = new Block (920, 680, 1);
-        b[76] = new Block (920, 710, 1);
+        b[72] = new Block (920, 590, 5);
+        b[73] = new Block (920, 620, 6);    
+        b[74] = new Block (920, 650, 6);
+        b[75] = new Block (920, 680, 6);
+        b[76] = new Block (920, 710, 6);
         
         
-        b[77] = new Block (1020, 530, 1);
-        b[78] = new Block (1020, 560, 1);
-        b[79] = new Block (1020, 590, 1);
-        b[80] = new Block (1020, 620, 1);
-        b[81] = new Block (1020, 650, 1);
-        b[82] = new Block (1020, 680, 1);
-        b[83] = new Block (1020, 710, 1);
+        b[77] = new Block (1020, 530, 5);
+        b[78] = new Block (1020, 560, 6);
+        b[79] = new Block (1020, 590, 6);
+        b[80] = new Block (1020, 620, 6);
+        b[81] = new Block (1020, 650, 6);
+        b[82] = new Block (1020, 680, 6);
+        b[83] = new Block (1020, 710, 6);
         
-        b[84] = new Block (1150, 470, 1);
-        b[85] = new Block (1150, 500, 1);
-        b[86] = new Block (1150, 530, 1);
-        b[87] = new Block (1150, 560, 1);
-        b[88] = new Block (1150, 590, 1);
-        b[89] = new Block (1150, 620, 1);
-        b[90] = new Block (1150, 650, 1);
-        b[91] = new Block (1150, 680, 1);
-        b[92] = new Block (1150, 710, 1);
+        b[84] = new Block (1150, 470, 5);
+        b[85] = new Block (1150, 500, 6);
+        b[86] = new Block (1150, 530, 6);
+        b[87] = new Block (1150, 560, 6);
+        b[88] = new Block (1150, 590, 6);
+        b[89] = new Block (1150, 620, 6);
+        b[90] = new Block (1150, 650, 6);
+        b[91] = new Block (1150, 680, 6);
+        b[92] = new Block (1150, 710, 6);
        
-        b[93] = new Block (1180, 470, 1);
-        b[94] = new Block (1180, 500, 1);
-        b[95] = new Block (1180, 530, 1);
-        b[96] = new Block (1180, 560, 1);
-        b[97] = new Block (1180, 590, 1);
-        b[98] = new Block (1180, 620, 1);
-        b[99] = new Block (1180, 650, 1);
-        b[100] = new Block (1180, 680, 1);
-        b[101] = new Block (1180, 710, 1);
+        b[93] = new Block (1180, 470, 6);
+        b[94] = new Block (1180, 500, 6);
+        b[95] = new Block (1180, 530, 6);
+        b[96] = new Block (1180, 560, 6);
+        b[97] = new Block (1180, 590, 6);
+        b[98] = new Block (1180, 620, 6);
+        b[99] = new Block (1180, 650, 6);
+        b[100] = new Block (1180, 680, 6);
+        b[101] = new Block (1180, 710, 6);
         
-        b[102] = new Block (1210, 470, 1);
-        b[103] = new Block (1210, 500, 1);
-        b[104] = new Block (1210, 530, 1);
-        b[105] = new Block (1210, 560, 1);
-        b[106] = new Block (1210, 590, 1);
-        b[107] = new Block (1210, 620, 1);
-        b[108] = new Block (1210, 650, 1);
-        b[109] = new Block (1210, 680, 1);
-        b[110] = new Block (1210, 710, 1);
-       
-        b[111] = new Block (1180, 440, 1);
+        b[102] = new Block (1210, 470, 6);
+        b[103] = new Block (1210, 500, 6);
+        b[104] = new Block (1210, 530, 6);
+        b[105] = new Block (1210, 560, 6);
+        b[106] = new Block (1210, 590, 6);
+        b[107] = new Block (1210, 620, 6);
+        b[108] = new Block (1210, 650, 6);
+        b[109] = new Block (1210, 680, 6);
+        b[110] = new Block (1210, 710, 6);
+      
+        b[111] = new Block (1180, 440, 5);
         
-        b[112] = new Block (1210, 380, 1);
-        b[113] = new Block (1210, 410, 1);
-        b[114] = new Block (1210, 440, 1);
+        b[112] = new Block (1210, 380, 5);
+        b[113] = new Block (1210, 410, 6);
+        b[114] = new Block (1210, 440, 6);
         
         //palikat ilmassa oikealta vasemmalle
         
-        b[115] = new Block (990, 330, 1);
-        b[116] = new Block (990, 360, 1);
-        b[117] = new Block (1020, 330, 1);
-        b[118] = new Block (1020, 360, 1);
+        b[115] = new Block (990, 330, 9);
+        b[116] = new Block (990, 360, 10);
+        b[117] = new Block (1020, 330, 7);
+        b[118] = new Block (1020, 360, 8);
         
-        b[119] = new Block (810, 340, 1);
-        b[120] = new Block (810, 370, 1);
-        b[121] = new Block (840, 340, 1);
-        b[122] = new Block (840, 370, 1);
+        b[119] = new Block (810, 340, 9);
+        b[120] = new Block (810, 370, 10);
+        b[121] = new Block (840, 340, 7);
+        b[122] = new Block (840, 370, 8);
         
-        b[123] = new Block (630, 340, 1);
-        b[124] = new Block (630, 370, 1);
-        b[125] = new Block (660, 340, 1);
-        b[126] = new Block (660, 370, 1);
+        b[123] = new Block (630, 340, 9);
+        b[124] = new Block (630, 370, 10);
+        b[125] = new Block (660, 340, 7);
+        b[126] = new Block (660, 370, 8);
         
         //kolmen sirkkelina alusta oikealta vasemmalle
         
-        b[127] = new Block (450, 340, 1);
-        b[128] = new Block (450, 370, 1);
-        b[129] = new Block (420, 340, 1);
-        b[130] = new Block (420, 370, 1);
-        b[131] = new Block (390, 340, 1);
-        b[132] = new Block (390, 370, 1);
-        b[133] = new Block (360, 340, 1);
-        b[134] = new Block (360, 370, 1);
-        b[135] = new Block (330, 340, 1);
-        b[136] = new Block (330, 370, 1);
-        b[137] = new Block (300, 340, 1);
-        b[138] = new Block (300, 370, 1);
-        b[139] = new Block (270, 340, 1);
-        b[140] = new Block (270, 370, 1);
-        b[141] = new Block (240, 340, 1);
-        b[142] = new Block (240, 370, 1);
-        b[143] = new Block (210, 340, 1);
-        b[144] = new Block (210, 370, 1);
-        b[145] = new Block (180, 340, 1);
-        b[146] = new Block (180, 370, 1);
-        b[147] = new Block (150, 340, 1);
-        b[148] = new Block (150, 370, 1);
+        b[127] = new Block (450, 340, 7);
+        b[128] = new Block (450, 370, 8);
+        b[129] = new Block (420, 340, 0);
+        b[130] = new Block (420, 370, 4);
+        b[131] = new Block (390, 340, 0);
+        b[132] = new Block (390, 370, 4);
+        b[133] = new Block (360, 340, 0);
+        b[134] = new Block (360, 370, 4);
+        b[135] = new Block (330, 340, 0);
+        b[136] = new Block (330, 370, 4);
+        b[137] = new Block (300, 340, 0);
+        b[138] = new Block (300, 370, 4);
+        b[139] = new Block (270, 340, 0);
+        b[140] = new Block (270, 370, 4);
+        b[141] = new Block (240, 340, 0);
+        b[142] = new Block (240, 370, 4);
+        b[143] = new Block (210, 340, 0);
+        b[144] = new Block (210, 370, 4);
+        b[145] = new Block (180, 340, 0);
+        b[146] = new Block (180, 370, 4);
+        b[147] = new Block (150, 340, 0);
+        b[148] = new Block (150, 370, 4);
         
         //koroke
         
-        b[149] = new Block (120, 280, 1);
-        b[150] = new Block (120, 310, 1);
-        b[151] = new Block (120, 340, 1);
-        b[152] = new Block (120, 370, 1);
-        b[153] = new Block (90, 280, 1);
+        b[149] = new Block (120, 280, 7);
+        b[150] = new Block (120, 310, 14);
+        b[151] = new Block (120, 340, 15);
+        b[152] = new Block (120, 370, 4);
+        b[153] = new Block (90, 280, 0);
         b[154] = new Block (90, 310, 1);
         b[155] = new Block (90, 340, 1);
-        b[156] = new Block (90, 370, 1);
-        b[157] = new Block (60, 280, 1);
+        b[156] = new Block (90, 370, 4);
+        b[157] = new Block (60, 280, 0);
         b[158] = new Block (60, 310, 1);
         b[159] = new Block (60, 340, 1);
-        b[160] = new Block (60, 370, 1);
-        b[161] = new Block (30, 280, 1);
+        b[160] = new Block (60, 370, 4);
+        b[161] = new Block (30, 280, 0);
         b[162] = new Block (30, 310, 1);
         b[163] = new Block (30, 340, 1);
-        b[164] = new Block (30, 370, 1);
-        b[165] = new Block (0, 280, 1);
+        b[164] = new Block (30, 370, 4);
+        b[165] = new Block (0, 280, 0);
         b[166] = new Block (0, 310, 1);
         b[167] = new Block (0, 340, 1);
-        b[168] = new Block (0, 370, 1);
+        b[168] = new Block (0, 370, 4);
         
-        b[169] = new Block (30, 250, 1);
+        b[169] = new Block (30, 250, 5);
         
-        b[170] = new Block (0, 190, 1);
-        b[171] = new Block (0, 220, 1);
-        b[172] = new Block (0, 250, 1);
+        b[170] = new Block (0, 190, 5);
+        b[171] = new Block (0, 220, 6);
+        b[172] = new Block (0, 250, 6);
         
         // ylimmät neliöpalikat vasemmalta oikealle
         
-        b[173] = new Block (210, 140, 1);
-        b[174] = new Block (180, 140, 1);
-        b[175] = new Block (210, 170, 1);
-        b[176] = new Block (180, 170, 1);
+        b[173] = new Block (210, 140, 7);
+        b[174] = new Block (180, 140, 9);
+        b[175] = new Block (210, 170, 8);
+        b[176] = new Block (180, 170, 10);
         
-        b[177] = new Block (390, 140, 1);
-        b[178] = new Block (360, 140, 1);
-        b[179] = new Block (390, 170, 1);
-        b[180] = new Block (360, 170, 1);
+        b[177] = new Block (390, 140, 7);
+        b[178] = new Block (360, 140, 9);
+        b[179] = new Block (390, 170, 8);
+        b[180] = new Block (360, 170, 10);
         
-        b[181] = new Block (570, 120, 1);
-        b[182] = new Block (540, 120, 1);
-        b[183] = new Block (570, 150, 1);
-        b[184] = new Block (540, 150, 1);
+        b[181] = new Block (570, 120, 7);
+        b[182] = new Block (540, 120, 9);
+        b[183] = new Block (570, 150, 8);
+        b[184] = new Block (540, 150, 10);
         
-        b[185] = new Block (750, 120, 1);
-        b[186] = new Block (720, 120, 1);
-        b[187] = new Block (750, 150, 1);
-        b[188] = new Block (720, 150, 1);
+        b[185] = new Block (750, 120, 7);
+        b[186] = new Block (720, 120, 9);
+        b[187] = new Block (750, 150, 8);
+        b[188] = new Block (720, 150, 10);
         
         // maalialusta
         
-        b[189] = new Block (930, 140, 1);
-        b[190] = new Block (930, 170, 1);
-        b[191] = new Block (960, 140, 1);
-        b[192] = new Block (960, 170, 1);
-        b[193] = new Block (990, 140, 1);
-        b[194] = new Block (990, 170, 1);
-        b[195] = new Block (1020, 140, 1);
-        b[196] = new Block (1020, 170, 1);
-        b[197] = new Block (1050, 140, 1);
-        b[198] = new Block (1050, 170, 1);
-        b[199] = new Block (1080, 140, 1);
-        b[200] = new Block (1080, 170, 1);
-        b[201] = new Block (1110, 140, 1);
-        b[202] = new Block (1110, 170, 1);
-        b[203] = new Block (1140, 140, 1);
-        b[204] = new Block (1140, 170, 1);
-        b[205] = new Block (1170, 140, 1);
-        b[206] = new Block (1170, 170, 1);
-        b[207] = new Block (1200, 140, 1);
-        b[208] = new Block (1200, 170, 1);
-        b[209] = new Block (1230, 140, 1);
-        b[210] = new Block (1230, 170, 1);
-        b[211] = new Block (1260, 140, 1);
-        b[212] = new Block (1260, 170, 1);
+        b[189] = new Block (930, 140, 9);
+        b[190] = new Block (930, 170, 10);
+        b[191] = new Block (960, 140, 0);
+        b[192] = new Block (960, 170, 4);
+        b[193] = new Block (990, 140, 0);
+        b[194] = new Block (990, 170, 4);
+        b[195] = new Block (1020, 140, 0);
+        b[196] = new Block (1020, 170, 4);
+        b[197] = new Block (1050, 140, 0);
+        b[198] = new Block (1050, 170, 4);
+        b[199] = new Block (1080, 140, 0);
+        b[200] = new Block (1080, 170, 4);
+        b[201] = new Block (1110, 140, 0);
+        b[202] = new Block (1110, 170, 4);
+        b[203] = new Block (1140, 140, 0);
+        b[204] = new Block (1140, 170, 4);
+        b[205] = new Block (1170, 140, 0);
+        b[206] = new Block (1170, 170, 4);
+        b[207] = new Block (1200, 140, 0);
+        b[208] = new Block (1200, 170, 4);
+        b[209] = new Block (1230, 140, 0);
+        b[210] = new Block (1230, 170, 4);
+        b[211] = new Block (1260, 140, 0);
+        b[212] = new Block (1260, 170, 4);
     }
 
     public void tick() {
--- src/Objects/Block.java
+++ src/Objects/Block.java
@@ -29,11 +29,103 @@ public class Block extends Rectangle {
     }
 
     public Image getBlockStyle() {
+        //tyhjä keskipala
          if(style == 1){
           ImageIcon blockStyle = new ImageIcon("src\\jamk\\fi\\MrGlass\\images\\grassworld\\Keskipala.png");
         return blockStyle.getImage();
         }
          
+         //oikea kulma
+        if(style == 2){
+          ImageIcon blockStyle = new ImageIcon("src\\jamk\\fi\\MrGlass\\images\\grassworld\\oikeakulma.png");
+        return blockStyle.getImage();
+        }
+        
+         //vasen kulma         
+        if(style == 3){
+          ImageIcon blockStyle = new ImageIcon("src\\jamk\\fi\\MrGlass\\images\\grassworld\\vasenkulma.png");
+        return blockStyle.getImage();
+        }
+        
+        // AlaSuora
+        if(style == 4){
+          ImageIcon blockStyle = new ImageIcon("src\\jamk\\fi\\MrGlass\\images\\grassworld\\AlaSuora.png");
+        return blockStyle.getImage();
+        }
+                
+        //kumpu
+        if(style == 5){
+          ImageIcon blockStyle = new ImageIcon("src\\jamk\\fi\\MrGlass\\images\\grassworld\\kumpu.png");
+        return blockStyle.getImage();
+        }
+        
+        //kummun korotuspala
+        if(style == 6){
+          ImageIcon blockStyle = new ImageIcon("src\\jamk\\fi\\MrGlass\\images\\grassworld\\kummunkorotus.png");
+        return blockStyle.getImage();
+        }
+        
+        // neliön oikea yläpala
+        if(style == 7){
+          ImageIcon blockStyle = new ImageIcon("src\\jamk\\fi\\MrGlass\\images\\grassworld\\nelioOikeaYlaKulma.png");
+        return blockStyle.getImage();
+        }
+        
+        //neliön oikea alapala
+        if(style == 8){
+          ImageIcon blockStyle = new ImageIcon("src\\jamk\\fi\\MrGlass\\images\\grassworld\\nelioOikeaAlaKulma.png");
+        return blockStyle.getImage();
+        }
+        
+        // neliön vasen yläpala
+        if(style == 9){
+          ImageIcon blockStyle = new ImageIcon("src\\jamk\\fi\\MrGlass\\images\\grassworld\\nelioVasenYlaKulma.png");
+        return blockStyle.getImage();
+        }
+        
+        // neliön vasen alapala
+        if(style == 10){
+          ImageIcon blockStyle = new ImageIcon("src\\jamk\\fi\\MrGlass\\images\\grassworld\\nelioVasenAlaKulma.png");
+        return blockStyle.getImage();
+        }
+        
+        //montunpohja
+        if(style == 11){
+          ImageIcon blockStyle = new ImageIcon("src\\jamk\\fi\\MrGlass\\images\\grassworld\\monttu.png");
+        return blockStyle.getImage();
+        }
+        
+        //vasen reuna
+        if(style == 12){
+          ImageIcon blockStyle = new ImageIcon("src\\jamk\\fi\\MrGlass\\images\\grassworld\\vasenReuna.png");
+        return blockStyle.getImage();
+        }
+
+        //vasen pystyseinä
+        if(style == 13){
+          ImageIcon blockStyle = new ImageIcon("src\\jamk\\fi\\MrGlass\\images\\grassworld\\pystysuoravasen.png");
+        return blockStyle.getImage();
+        }
+        
+        //oikea pystyseinä
+        if(style == 14){
+          ImageIcon blockStyle = new ImageIcon("src\\jamk\\fi\\MrGlass\\images\\grassworld\\pystysuoraoikea.png");
+        return blockStyle.getImage();
+        }
+        
+        //vasen liitospala
+        if(style == 15){
+          ImageIcon blockStyle = new ImageIcon("src\\jamk\\fi\\MrGlass\\images\\grassworld\\Pienkulma.png");
+        return blockStyle.getImage();
+        }
+        
+        //oikea reuna
+        if(style == 16){
+          ImageIcon blockStyle = new ImageIcon("src\\jamk\\fi\\MrGlass\\images\\grassworld\\oikeaReuna.png");
+        return blockStyle.getImage();
+        }
+
+        // nollalla suora 
          else {
         ImageIcon blockStyle = new ImageIcon("src\\jamk\\fi\\MrGlass\\images\\grassworld\\suora.png");
         return blockStyle.getImage();
